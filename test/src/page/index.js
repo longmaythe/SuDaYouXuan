b3d3efd025a96941b3fd54af89515a70a7582f65
longmaythe <2586384176@qq.com>
2019-12-11 16:33:15 +0800

    change

--- test/src/page/index.js
+++
@@ -1,9 +0,0 @@
-import ClassIfy from "./classify"
-import Home from "./home"
-import My from "./my"
-import ShoppingCar from "./shoppingCar"
-import NotFound from './404'
-import Content from './classify/content/content'
-import localList from './classify/content/localList'
-
-export {ClassIfy,Home,My,ShoppingCar,NotFound,Content,localList}
